bdc506c3e4b07affdc1317ca4e4146a6d5846d1c
abhishek2097 <abhishek2097@gmail.com>
2020-05-18 10:54:05 +0530

    Changes

---
+++ Workfield/Spring_Security_Form_Based_Auth_Demo/src/main/java/com/abhi/app/SpringSecurityFormBasedAuthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.abhi.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityFormBasedAuthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityFormBasedAuthDemoApplication.class, args);
+	}
+
+}
